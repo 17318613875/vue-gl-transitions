42c51a33d13e912fb165e177dacf82a58715f790
彭斌斌 <binbin@mgtv.com>
2020-06-30 17:47:56 +0800

    feat(util.ts): 添加 工具模块

---
+++ src/components/util.ts
@@ -0,0 +1,176 @@
+import GLTransitions from "gl-transitions";
+import createTexture from "gl-texture2d";
+export type Texture = ReturnType<typeof createTexture>;
+export type Path = string;
+export type Source =
+  | ImageData
+  | HTMLImageElement
+  | HTMLVideoElement
+  | HTMLCanvasElement;
+export type Transitions = {
+  name: string;
+  glsl: string;
+};
+export type DefaultParams = {
+  [key: string]: number | string;
+};
+type That = {
+  progressSync: number;
+  glContext: WebGLRenderingContext | void;
+  GlFrom: Texture;
+  GlTo: Texture;
+};
+type ContCextAttributes = {
+  alpha?: boolean;
+  antialias?: boolean;
+  depth?: boolean;
+  failIfMajorPerformanceCaveat?: boolean;
+  powerPreference?: "default" | "high-performance" | "low-power";
+  premultipliedAlpha?: boolean;
+  preserveDrawingBuffer?: boolean;
+  stencil?: boolean;
+};
+
+/**
+ * @description 设置跨域资源
+ * @param src 资源路径
+ * @param target HTMLVideoElement | HTMLImageElement
+ */
+export function SetCrossOrigin(
+  src: string,
+  target: HTMLVideoElement | HTMLImageElement
+) {
+  new URL(src, window.location.href).origin !== window.location.origin &&
+    (target.crossOrigin = "anonymous");
+}
+
+/**
+ * @description 获取transition
+ * @param transitionName string transitions name
+ */
+export const GetTransition = (
+  transitionName: string,
+  defaultParams: DefaultParams
+): Transitions => {
+  const transition = GLTransitions.find(({ name }) => transitionName === name);
+  if (transition) {
+    transition.defaultParams &&
+      Object.keys(transition.defaultParams).forEach((keyName) => {
+        const val = defaultParams[keyName];
+        val !== undefined &&
+          transition.defaultParams &&
+          (transition.defaultParams[keyName] = val);
+      });
+    return transition;
+  } else {
+    throw new Error(`未匹配到可用过渡模板：${transitionName}`);
+  }
+};
+
+/**
+ * @description 创建canvas 的上下文
+ * @param canvas HTMLCanvasElement
+ * @param opts 上下文属性
+ */
+export function GetContext(
+  canvas: HTMLCanvasElement,
+  opts?: ContCextAttributes
+) {
+  const opts_ = Object.assign(opts || {}, {
+    powerPreference: "default",
+  });
+  return (
+    canvas.getContext("webgl", opts_) ||
+    canvas.getContext("webgl-experimental", opts_) ||
+    canvas.getContext("experimental-webgl", opts_)
+  );
+}
+
+/**
+ * @description 通过地址加载图片
+ * @param src 图片地址
+ */
+export const LoadImg = (src: string): Promise<HTMLImageElement> => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    SetCrossOrigin(src, img);
+    img.onload = () => resolve(img);
+    img.onerror = reject;
+    img.src = src;
+  });
+};
+
+/**
+ * @description 通过HTML Element 加载视频
+ * @param video HTMLVideoElement
+ */
+interface EventR extends EventTarget {
+  reqId: number;
+}
+interface VideoR extends HTMLVideoElement {
+  reqId: number;
+}
+function startTracking(
+  { target }: Event,
+  that: That,
+  keyName: "GlFrom" | "GlTo"
+) {
+  if (target) {
+    (target as EventR).reqId = requestAnimationFrame(function play() {
+      that[keyName] = createTexture(
+        that.glContext as WebGLRenderingContext,
+        target as HTMLVideoElement
+      );
+      that.progressSync = (target as HTMLVideoElement).currentTime;
+      (target as EventR).reqId = requestAnimationFrame(play);
+    });
+  }
+}
+export const LoadVideo = (
+  video: HTMLVideoElement,
+  that: That,
+  keyName: "GlFrom" | "GlTo"
+): Promise<HTMLVideoElement> => {
+  return new Promise((resolve, reject) => {
+    SetCrossOrigin(video.src, video);
+    video.onloadeddata = () => resolve(video);
+    video.onplay = (event) => startTracking(event, that, keyName);
+    video.onpause = () => cancelAnimationFrame((video as VideoR).reqId);
+    video.onerror = reject;
+  });
+};
+
+/**
+ * @description 加载资源
+ * @param source
+ */
+export async function LoadSource(
+  source: Source,
+  that: object,
+  keyName: "GlFrom" | "GlTo"
+): Promise<Source> {
+  let target!: Source;
+  if (typeof source === "string") {
+    if (/\.(jpg|png|jpeg)$/.test(source)) {
+      target = (await LoadImg(source)) as HTMLImageElement;
+    }
+  } else {
+    const tagName = (source as HTMLElement).tagName;
+    switch (tagName) {
+      case "VIDEO":
+        target = await LoadVideo(
+          source as HTMLVideoElement,
+          that as That,
+          keyName
+        );
+        break;
+      case "IMG":
+        target = source as HTMLImageElement;
+        break;
+      case "CANVAS":
+        target = source as HTMLCanvasElement;
+        break;
+    }
+  }
+  return target;
+}
