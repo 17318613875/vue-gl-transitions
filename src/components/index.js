1764386ecc0e4ad1373fa3a11f1c2ef9f4643af5
彭斌斌 <binbin@mgtv.com>
2020-07-01 09:44:55 +0800

    docs(package*.*): 修改 发布配置文件

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import VueGlTransition from "./VueGlTransition.vue";
+
+VueGlTransition.install = function(Vue) {
+  Vue.component(VueGlTransition.name, VueGlTransition);
+};
+
+export default VueGlTransition;
