6bc784ef8a4e2d2872df16c4e6bc36f438896212
彭斌斌 <binbin@mgtv.com>
2020-07-01 15:36:21 +0800

    docs(vue-gl-transitions.d.ts): 修改 vue-gl-transitions.d.ts export class

--- types/vue-gl-transitions.d.ts
+++ types/vue-gl-transitions.d.ts
@@ -2,12 +2,10 @@ import Vue from "vue";
 
 declare function install(vue: typeof Vue): void;
 
-declare class VueGlTransition extends Vue {}
+export declare class VueGlTransition extends Vue {}
 
 declare const _default: {
   install: typeof install;
 };
-
-export { VueGlTransition };
 
 export default _default;
